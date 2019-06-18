98d74d715c11f63f8608f9eb55a83c02d1d0c3a0
Your Name <11@111.com>
2019-06-18 18:12:54 +0800

    0618商品信息

--- src/api/index.js
+++ src/api/index.js
@@ -2,8 +2,9 @@
 //根据接口文档编写
 //调用自定义ajax请求函数请求
 //每个函数的返回值都是Promise对象
-
+import jsonp from 'jsonp'
 import ajax from "./ajax";
+import { message } from 'antd';
 const BASE = ''
 
 //1.登录
@@ -15,8 +16,31 @@ export const reqLogin = (username,password) => ajax(BASE+ '/login',{username,pas
 //添加用户
 export const reqAddUser = (user) => ajax(BASE+'/manage/user/add',user,'POST')
 
+//获取分类列表
+export const reqCategorys = (parentId) => ajax(BASE + '/manage/category/list', {parentId})
+
 
 //测试
-reqLogin('admin','admin').then(result =>{
-    console.log('result',result)
-})
+// reqLogin('admin','admin').then(result =>{
+//     console.log('result',result)
+// })
+
+export const reqWeather =(location)=>{
+    const url =  `http://api.map.baidu.com/telematics/v3/weather?location=${location}&output=json&ak=3p49MVra6urFRGOT9s8UBWr2`
+    return new Promise((resolve,reject) => {
+      setTimeout(()=>{
+        
+        jsonp(url,{},(error,data)=>{
+            if(!error && data.status ==='success'){
+                const {dayPictureUrl,weather }
+                = data.results[0].weather_data[0]
+                resolve({dayPictureUrl,weather})
+            }else{
+                message.error('获取天气信息！')
+            }
+        })
+
+      },2000)
+    })
+ 
+}
--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -4,13 +4,118 @@ Admin头部界面
 
 import React, { Component } from 'react'
 import './index.less'
+import {withRouter} from 'react-router-dom'
+import {formateDate} from '../../utils/dataUtils'
+import menuList from '../../config/menuConfig'
+import memoryUtils from '../../utils/memoryUtils';
+import  {removeUser} from '../../utils/storageUtils'
+import { reqWeather } from '../../api';
+import { Modal } from 'antd'
+import Linkbutton from '../../components/link-button'
+import LinkButton from '../../components/link-button';
 
-export default class Header extends Component {
+
+class Header extends Component {
+
+  static propTypes ={}
+
+//初始化状态
+state = {
+  currentTime:formateDate(Date.now()), //当前时间字符串
+  dayPictureUrl:'',//天气图片的url
+  weather:'', //天气文本
+}
+
+showCurrentiem =()=> {
+  setInterval(()=>{
+    const currentTime = formateDate(Date.now())
+    this.setState({
+      currentTime
+    })
+  },1000);
+}
+
+   getTile =()=>{
+     const path = this.props.location.pathname
+     let title=''
+     menuList.forEach(item =>{
+       if(item.key === path){
+         title = item.title
+       }else if(item.children){
+        const cItem = item.children.find(item=>item.key === path)
+         if(cItem){
+            title = cItem.title
+         }
+       }
+     })
+      return title
+   }
+ 
+   getWeather = async () =>{
+     const {dayPictureUrl,weather} = await reqWeather('北京')
+        this.setState({
+          dayPictureUrl,
+          weather
+        })
+   }
+ 
+logout =()=> {
+   //显示确认框
+   Modal.confirm({
+    title: '你确定要退出吗?',
+    onOk:()=> {
+      console.log('确定');
+      //清除保存user
+       removeUser()
+       memoryUtils.user = {}
+      //跳转登录
+      this.props.history.replace('/login')
+    },
+    onCancel() {
+      console.log('取消');
+    },
+   })
+}
+
+
+//在组件即将销毁之前做收尾工作：清除定时器
+componentWillUnmount(){
+ clearInterval(this.itervalId)
+}
+
+ componentDidMount(){
+   //每隔1s更新时间显示
+   this.showCurrentiem()
+   //获取天气
+   this.getWeather()
+   
+ }
+
+
   render() {
+   const {currentTime,dayPictureUrl,weather} = this.state
+     
+   const {user} =memoryUtils
+   const title = this.getTile()
+
     return (
       <div className="header">
-        Header
+        <div className="header-top">
+        <span>欢迎,{user.username}</span>
+        {/* <a href="#" onClick={this.logout}>退出</a> */}
+        <LinkButton onClick={this.logout}>退出</LinkButton>
+        </div>
+       
+        <div className="header-bottom">
+          <div className="header-bottom-left">{title}</div>
+          <div className="header-bottom-right">
+          <span>{currentTime}</span>
+          {!!dayPictureUrl && <img src={dayPictureUrl} alt="weather"/>}
+          <span>{weather}</span>
+          </div>
+        </div>
       </div>
     )
   }
 }
+export default withRouter(Header)
